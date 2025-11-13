6a02ea07292485635d685c865845ac42aef280f6
David Edward Hinds <davideh@vt.edu>
2025-11-13 14:31:24 -0500

    Add ROxygen comment.

--- R/get_subj_est_ln_k.R
+++ R/get_subj_est_ln_k.R
@@ -1,4 +1,9 @@
-# Return ln(k) estimates for each subject
+#' Return ln(k) estimates for each subject
+#'
+#' @param dd_data A specially formatted data frame as returned by the
+#' prepare_data_frame function.
+#' @returns  A data frame consisting of one observation per subject within group
+#' with the variable ln_k containing that subject's estimated ln_k value.
 get_subj_est_ln_k <- function(dd_data){
   if("study" %in% names(dd_data)){
     result <- dd_data %>%
